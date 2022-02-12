0addb9aa3b1442efd5a8c0f6717ecbf2b45fa30b
Alexander <alexndegwa46@gmail.com>
2022-02-12 21:16:08 +0300

    installed sql

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/c/Users/antho/Documents/blog/blog.db'
-
 db = SQLAlchemy(app)
 
 class Blogpost(db.Model):
