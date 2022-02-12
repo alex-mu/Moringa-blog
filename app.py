d707025d82e8e6830a55bdc99604ec42d0ff073b
Alexander <alexndegwa46@gmail.com>
2022-02-12 22:02:25 +0300

    created cofig file

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////users/alexandermureithi/Documents/blog/moringablog.db'
 db = SQLAlchemy(app)
 
 class Blogpost(db.Model):
